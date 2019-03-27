b2e83e37e7e8a47a7439d425cfa2deef96718429
Matheus <matheus.ferreira@lbca.com.br>
2019-03-27 09:21:08 -0300

    aprendendo sobre o this(referencia do proprio objeto)

---
+++ Classes/Sobrecargar/Sobrecargas/Sobrecargas/ProblemaProduto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sobrecargas
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 10;
+        }
+
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + "," + Quantidade + "unidade , Total: $" + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture
+                );
+        }
+    }
+}
---
+++ Classes/Sobrecargar/Sobrecargas/Sobrecargas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sobrecargas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                Sobrecargar é um recurso que uma classe possui de oferever mais de uma operacao com o mesmo nome, 
+                porem com diferentes listas de parametros.
+            */
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            /*
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            */
+            /*
+            sintax alternativa 
+
+                Produto p;
+                p = new Produto()
+                {
+                    Nome = "TV",
+                    Preco = 900.00,
+                    Quantidade = 30;
+                }              
+            */
+
+            Produto p;
+            p = new Produto(nome, preco);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+        }
+    }
+}
