5e24d1263a896d562322ddd73042f7509ea86ca4
Matheus <matheus.ferreira@lbca.com.br>
2019-03-28 09:04:56 -0300

    aprendendo encapsulamento

---
+++ Classes/Encapsulamentos/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * Encapsulamento e um principio que consiste em esconder detalhes um componente, expondo apenas
+                * operacoes seguras e que o mantenham em um estado consister (a propria claase deve fazer isso)
+                * Todo atributo e definido como private e implementa-se methosdos get(obeter) e set(definir) como regra de negocio
+            */
+
+            Produto p;
+            p = new Produto("TV", 500.00, 10);
+            p.SetNome("T");
+
+            Console.WriteLine(p.GetNome());
+        }
+    }
+}
