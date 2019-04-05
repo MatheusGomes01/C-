c4b95cae034640814f161af0f822d1775064b568
Matheus <matheus.ferreira@lbca.com.br>
2019-04-05 09:01:08 -0300

    properties e autoproperties

---
+++ Classes/AutoProperties/AutoProperties/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace AutoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * AutoProperties é uma forma simplificada de se declarar propriedades que nao necessitam 
+                * logicas particularespara operacoes get e set               
+                * 
+                * Ordem sugerida na OO
+                * 
+                * Atributos Privativos;
+                * Propriedades autoimplementadas;
+                * Contrutores;
+                * Propriedades Customizadas;
+                * outros metodos de classe;
+            */
+
+
+            Produto p;
+            p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
---
+++ Classes/properties/properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * Properties são definicoes de metodos encapsulados, porem expondo sintaxe similar a de atributos e nao 
+                * de metodos
+            */
+
+            Produto p;
+            p = new Produto("TV", 500.00, 10 );
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
