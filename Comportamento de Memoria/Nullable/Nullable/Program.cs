251d9d0a6738564127e19b00548b72c7e62268a1
Matheus <matheus.ferreira@lbca.com.br>
2019-04-11 08:12:31 -0300

    Nullable

---
+++ Comportamento de Memoria/Nullable/Nullable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+            //ou double? x = null
+
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); // traz o valor defaul da variavel ou o setado
+            Console.WriteLine(y.GetValueOrDefault()); // traz o valor defaul da variavel ou o setado
+
+            Console.WriteLine(x.HasValue);// indica se tem valor ou nao (true/false)
+            Console.WriteLine(x.HasValue);// indica se tem valor ou nao (true/false)
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X é nulo");
+            }
+
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y é nulo");
+            }
+
+            Console.WriteLine(y = x ?? 15.00);
+        }
+    }
+}
