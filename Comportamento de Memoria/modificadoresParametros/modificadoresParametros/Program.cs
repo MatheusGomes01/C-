ecd871ccd215bb0ab90eb0dac50814e6ff3c10a3
indio.ferreira <matheus.ferreira@lbca.com.ferreira>
2019-04-15 09:13:53 -0300

    ref e out >> sao code smells, ou seja, design ruim. Evitar usar

--- Comportamento de Memoria/modificadoresParametros/modificadoresParametros/Program.cs
+++ Comportamento de Memoria/modificadoresParametros/modificadoresParametros/Program.cs
@@ -12,9 +12,18 @@ namespace modificadoresParametros
         {
             int s1 = Calculator.Sum(2, 3);
             int s2 = Calculator.Sum(2, 4, 3);
+            int a = 10;
+            int b = 10;
+            int triple;
+
+            Calculator.Tiple(ref a);
+            Calculator.Tiple(b, out triple);
 
             Console.WriteLine(s1);
             Console.WriteLine(s2);
+            Console.WriteLine(a);
+            Console.WriteLine(triple);
+
 
         }
     }
--- Comportamento de Memoria/modificadoresParametros/modificadoresParametros/calculator.cs
+++ Comportamento de Memoria/modificadoresParametros/modificadoresParametros/calculator.cs
@@ -17,5 +17,15 @@ class Calculator
             }
             return sum;
         }
+
+        public static void Tiple(ref int x) //ref é para tornar o x para a variavel original
+        {
+            x = x * 3;
+        }
+
+        public static void Tiple(int origin, out int result) //out faz o param ser uma referencia para a variavel original e permite que a variavel original nao precise ser iniciada
+        {
+            result = origin * 3;
+        }
     }
 }
