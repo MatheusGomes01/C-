e0cc19d560d2a68e21f94a8b20ce8f422fb60fbc
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-05-10 07:55:11 -0300

    Versionamento

---
+++ Execrcicios de fixacao/CourseOrder/Entities/OrderItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseOrder.Entities
+{
+    class OrderItem
+    {
+        public int Quantify { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantify, double price, Product product)
+        {
+            Quantify = quantify;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantify;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantify: "
+                + ", Subtotal: $ "
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
