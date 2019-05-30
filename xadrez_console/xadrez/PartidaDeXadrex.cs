0c575ec31cc85635a243e2d35525cbad2133048d
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-05-29 21:19:49 -0300

    jogada especial roque pequeno e grande

--- xadrez_console/xadrez/PartidaDeXadrex.cs
+++ xadrez_console/xadrez/PartidaDeXadrex.cs
@@ -29,9 +29,9 @@ namespace xadrez
             colocarPecas();
         }
 
-        public Peca executaMovimento(Posicao oritem, Posicao destino)
+        public Peca executaMovimento(Posicao origem, Posicao destino)
         {
-            Peca p = tab.retirarPeca(oritem);
+            Peca p = tab.retirarPeca(origem);
             p.incrementarQtdMovimento();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
@@ -39,6 +39,26 @@ namespace xadrez
             if (pecaCapturada != null)
             {
                 capturadas.Add(pecaCapturada);
+            }
+
+            //#jogada especial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemTorre);
+                T.incrementarQtdMovimento();
+                tab.colocarPeca(T, destinoTorre);
+            }
+
+            //#jogada especial roque grande 
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemTorre);
+                T.incrementarQtdMovimento();
+                tab.colocarPeca(T, destinoTorre);
             }
             return pecaCapturada;
         }
@@ -53,6 +73,26 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             tab.colocarPeca(p, origem);
+
+            //#jogada especial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoTorre);
+                T.incrementarQtdMovimento();
+                tab.colocarPeca(T, origemTorre);
+            }
+
+            //#jogada especial roque grande 
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(destinoTorre);
+                T.incrementarQtdMovimento();
+                tab.colocarPeca(T, origemTorre);
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -237,7 +277,7 @@ namespace xadrez
             colorcarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
             colorcarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
             colorcarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
-            colorcarNovaPeca('e', 1, new Rei(tab, Cor.Branca));
+            colorcarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
             colorcarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colorcarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colorcarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
@@ -255,7 +295,7 @@ namespace xadrez
             colorcarNovaPeca('b', 8, new Cavalo(tab, Cor.Preta));
             colorcarNovaPeca('c', 8, new Bispo(tab, Cor.Preta));
             colorcarNovaPeca('d', 8, new Dama(tab, Cor.Preta));
-            colorcarNovaPeca('e', 8, new Rei(tab, Cor.Preta));
+            colorcarNovaPeca('e', 8, new Rei(tab, Cor.Preta, this));
             colorcarNovaPeca('f', 8, new Bispo(tab, Cor.Preta));
             colorcarNovaPeca('g', 8, new Cavalo(tab, Cor.Preta));
             colorcarNovaPeca('h', 8, new Torre(tab, Cor.Preta));
