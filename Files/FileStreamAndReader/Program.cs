1f8fa00726f090b2674ce7a6a78ac497215c79bc
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-05-30 08:22:20 -0300

    aprendendo sobre files, fileStream e StreamReader

---
+++ Files/FileStreamAndReader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileStreamAndReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*fileStream é uma classe que disponibiliza uma stream associada a um arquivo, 
+             * permitindo operacoes de leitura e escrita e é uma stream de caracteres
+             * streamReader é uma stream ou seja uma sequencia de dados, 
+             * usado para transmissao de dados em sequencia e é uma stream binária*/
+
+            string path = @"C:\Users\Matheus\Downloads\file.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Files/Introduction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\Matheus\Downloads\file.txt";
+            string targetPath = @"C:\Users\Matheus\Downloads\file1.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred"); ;
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
