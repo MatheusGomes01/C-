846bf4f028b511500de3bf7bb08769e96b80786f
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-06-21 07:40:42 -0300

    versionamento

---
+++ CourseExtensionsMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CourseExtensionsMethods.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisOnj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisOnj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours" ;
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ CourseExtensionsMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CourseExtensionsMethods.Extensions;
+
+namespace CourseExtensionsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             sao metodos que estendem a funcionalidade de um tipo, sem precisar alterar o codigo fonte deste tipo,
+             nem herdar desse tipo
+
+            Extension Method é um alcasse estatica, com método estatico, onde o primeiro parametro deste metodo
+            é o this, seguindo da declaracao de um parametro do tipo que deseja estender.
+             */
+
+            /*
+                DateTime dt = new DateTime(2019, 6, 20, 8, 10, 45);
+                Console.WriteLine(dt.ElapsedTime());
+            */
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
