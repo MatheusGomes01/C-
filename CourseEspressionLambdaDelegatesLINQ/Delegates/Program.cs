926e4b9a91d20c1b2352a61e7d8058c2475ac8b7
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-06-21 08:14:45 -0300

    versionamento

---
+++ CourseEspressionLambdaDelegatesLINQ/Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               É uma referencia (com type safety) para um ou mais metodos
+               uso comum em comunicacao entre objetos de forma flexivel e extensivel(eventos/callbacks)
+               parametrizacao de operacao por metodos(programacao funcional)
+             */
+        }
+    }
+}
