decbe1ac7a44c9311bdd5a5a58600e8bed8b7efb
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-06-24 07:48:45 -0300

    versionamento

--- CourseEspressionLambdaDelegatesLINQ/Delegates/Program.cs
+++ CourseEspressionLambdaDelegatesLINQ/Delegates/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using Delegates.Services;
 
 namespace Delegates
 {
     class Program
     {
+
+        delegate double BinaryNumericOperation(double n1, double n2);
         static void Main(string[] args)
         {
             /*
                É uma referencia (com type safety) para um ou mais metodos
                uso comum em comunicacao entre objetos de forma flexivel e extensivel(eventos/callbacks)
                parametrizacao de operacao por metodos(programacao funcional)
+
+               Action, Func, Predicate
              */
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+           
+            double result = CalculationService.Square(a);
+            Console.WriteLine(result);
+            Console.WriteLine(op(a, b));
+
         }
     }
 }
