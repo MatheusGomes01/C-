43c3d35171599e317bb082a063071cab1bbdb3ec
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2019-06-25 18:58:50 -0300

    versionamento

---
+++ CourseEspressionLambdaDelegatesLINQ/CourseFunc/Program.cs
@@ -0,0 +1,36 @@
+﻿using CourseFunc.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CourseFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //representa um metodo que recebe zero ou mais argumentos, e retorna um valor, ele tem 16 sobrecargas
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD", 80.90));
+            Func<Product, string> func = NameUpper;
+
+            //List<string> result = list.Select(NameUpper).ToList();
+            //List<string> result = list.Select(func).ToList();
+
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
---
+++ CourseEspressionLambdaDelegatesLINQ/LINQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             É um conjunto de tecnologis baseadas na integracao de funcionalidades de consulta diretamenta na linguagem C#
+             operacaoes chamadas diretamenta a partir das colecoes;
+             consultas sao objetos de prmeira classe;
+             Suporte do compilador de intelliSense da IDE
+
+            Namespace: System.Linq;
+
+            Possui diversas operacoes de consulta, cujos parametros tipicamente sao expressoes lambda ou expressoes de sintaxe similar a SQL
+
+            para trabalhar com LINQ tem que criar um data source (colecao, array, recurso de E/S, ect.);
+            definir a query;
+            Executar a query (Foreach ou alguma operacao terminal);
+
+             */
+
+            // specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define th equery expression
+            var result = numbers.Where(x => x % 2  == 0).Select(x => x * 10);
+
+            // Execute the query 
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
